f5c3afa2566d6acc550275cded22a8e050f9e262
Gscienty <gaoxiaochuan@hotmail.com>
2019-03-14 17:45:55 +0800

    add refund required field 'out_refund_no'

--- src/refund.py
+++ src/refund.py
@@ -1,22 +1,22 @@
 from . import app, inspect
 
 @app.app.route('/micro-pay/refund')
-@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'total_fee', 'refund_fee' })
+@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'out_refund_no', 'total_fee', 'refund_fee' })
 def micro_pay_reverse():
     return app.process('https://api.mch.weixin.qq.com/secapi/refund', sec=True)
 
 @app.app.route('/jsapi/refund')
-@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'total_fee', 'refund_fee' })
+@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'out_refund_no', 'total_fee', 'refund_fee' })
 def jsapi_pay_reverse():
     return app.process('https://api.mch.weixin.qq.com/secapi/refund', sec=True)
 
 @app.app.route('/native/refund')
-@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'total_fee', 'refund_fee' })
+@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'out_refund_no', 'total_fee', 'refund_fee' })
 def native_pay_reverse():
     return app.process('https://api.mch.weixin.qq.com/secapi/refund', sec=True)
 
 @app.app.route('/app/refund')
-@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'total_fee', 'refund_fee' })
+@inspect.fields({ [ 'transaction_id', 'out_trade_no' ], 'out_refund_no', 'total_fee', 'refund_fee' })
 def app_pay_reverse():
     return app.process('https://api.mch.weixin.qq.com/secapi/refund', sec=True)
 
