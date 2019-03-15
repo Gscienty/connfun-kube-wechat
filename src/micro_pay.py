7ba1668592698eb4b2a6d511474383b9988180cd
Gscienty <gaoxiaochuan@hotmail.com>
2019-03-15 17:29:40 +0800

    add micropay http request method

--- src/micro_pay.py
+++ src/micro_pay.py
@@ -1,6 +1,6 @@
 import app, app_inspect
 
-@app.app.route('/micro-pay')
+@app.app.route('/micro-pay', methods=[ 'POST' ])
 @app_inspect.fields({ 'body', 'out_trade_no', 'total_fee', 'spbill_create_ip', 'auth_code' })
 def micro_pay():
     return app.process('https://api.mch.weixin.qq.com/pay/micropay')
