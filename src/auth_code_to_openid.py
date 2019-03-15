97fa6ade10bf6b33ff943c1920cf825584947af1
Gscienty <gaoxiaochuan@hotmail.com>
2019-03-15 17:29:12 +0800

    add close-order http request method

--- src/auth_code_to_openid.py
+++ src/auth_code_to_openid.py
@@ -1,6 +1,6 @@
 import app, app_inspect
 
-@app.app.route('/micro-pay/authcodetopenid')
+@app.app.route('/micro-pay/authcodetopenid', methods=[ 'POST' ])
 @app_inspect.fields({ 'auth_code' })
 def micro_pay_auth_code_to_openid():
     return app.process('https://api.mch.weixin.qq.com/tools/authcodetoopenid')
--- src/close_order.py
+++ src/close_order.py
@@ -1,26 +1,26 @@
 import app, app_inspect
 
-@app.app.route('/jsapi/close-order')
+@app.app.route('/jsapi/close-order', methods=[ 'POST' ])
 @app_inspect.fields({ 'out_trade_no' })
 def jsapi_close_order():
     return app.process('https://api.mch.weixin.qq.com/pay/closeorder')
 
-@app.app.route('/native/close-order')
+@app.app.route('/native/close-order', methods=[ 'POST' ])
 @app_inspect.fields({ 'out_trade_no' })
 def native_close_order():
     return app.process('https://api.mch.weixin.qq.com/pay/closeorder')
 
-@app.app.route('/app/close-order')
+@app.app.route('/app/close-order', methods=[ 'POST' ])
 @app_inspect.fields({ 'out_trade_no' })
 def app_close_order():
     return app.process('https://api.mch.weixin.qq.com/pay/closeorder')
 
-@app.app.route('/h5/close-order')
+@app.app.route('/h5/close-order', methods=[ 'POST' ])
 @app_inspect.fields({ 'out_trade_no' })
 def h5_close_order():
     return app.process('https://api.mch.weixin.qq.com/pay/closeorder')
 
-@app.app.route('/micro-app/close-order')
+@app.app.route('/micro-app/close-order', methods=[ 'POST' ])
 @app_inspect.fields({ 'out_trade_no' })
 def micro_app_close_order():
     return app.process('https://api.mch.weixin.qq.com/pay/closeorder')
