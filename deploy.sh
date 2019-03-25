54bc175766148cf4414f1ec31b9b47d1862d3f8c
Gscienty <gaoxiaochuan@hotmail.com>
2019-03-25 10:31:12 -0400

    add RUN_ENV environment var

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build . -t connfun.com/wechat-pay:latest
-kubectl apply -f deployment.yaml
--- src/http_util.py
+++ src/http_util.py
@@ -1,6 +1,7 @@
 import xml_util
 import requests
 import os
+import wechat_sec
 
 __headers = { 'Content-Type': 'application/xml' }
 
@@ -13,10 +14,11 @@ def normal_call(uri, content):
 
 def security_call(uri, content):
     global __headers
-
-    res = requests.post(uri,
-                        data=xml_util.to_xml(content),
-                        headers=__headers,
-                        cert=(os.environ['API_CERT'], os.environ['API_KEY']))
-    return xml_util.parse_xml(res.text)
-
+    if os.environ['RUN_ENV'] in { 'release', 'develop' }:
+        res = requests.post(uri,
+                data=xml_util.to_xml(content),
+                headers=__headers,
+                cert=(os.environ['API_CERT'], os.environ['API_KEY']))
+        return xml_util.parse_xml(res.text)
+    elif os.environ['RUN_ENV'] in { 'mock' }:
+        return wechat_sec.mock_res_build()
