c8f76327ca9930d46924dd22aebd8e51c4ff03b3
Gscienty <gaoxiaochuan@hotmail.com>
2019-03-25 11:40:16 -0400

    add log

--- src/app.py
+++ src/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, abort, jsonify
 import wechat_sec, http_util
+import os
 
 app = Flask(__name__)
 
 def process(uri, sec=False):
+    run_env = os.environ['RUN_ENV']
+    req = wechat_sec.req_build(request.json)
+    if run_env in { 'develop', 'mock' }:
+        app.logger.debug('client request: uri: {uri};sec:{sec}; req body: {req}'.format(uri=uri, sec=sec, req=str(req)))
     if sec:
-        res_content = http_util.security_call(uri,
-                wechat_sec.req_build(request.json))
+        res_content = http_util.security_call(uri, req)
     else:
-        res_content = http_util.normal_call(uri,
-                wechat_sec.req_build(request.json))
+        res_content = http_util.normal_call(uri, req)
 
     if 'sign' not in res_content:
         return jsonify(res_content), 400
--- src/xml_util.py
+++ src/xml_util.py
@@ -1,5 +1,7 @@
 import xml
 from bs4 import BeautifulSoup
+import os
+import app
 
 def to_xml(req_content):
     xml_items = []
@@ -10,11 +12,17 @@ def to_xml(req_content):
         if k == 'detail' and not v.startswith('<![CDATA['):
             v = '<![CDATA[{}]]'.format(v)
         xml_items.append('<{key}>{value}</{key}>'.format(key=k, value=v))
-    return '<xml>{}</xml>'.format(''.join(xml_items))
+    res = '<xml>{}</xml>'.format(''.join(xml_items))
+    if os.environ['RUN_ENV'] in { 'mock', 'develop' }:
+        app.app.logger.debug(res)
+    return res
 
 def parse_xml(xml_content):
     soup = BeautifulSoup(xml_content, features='xml')
     xml = soup.find('xml')
     if not xml:
         return {}
-    return dict([(item.name, item.text) for item in xml.find_all()])
+    res = dict([(item.name, item.text) for item in xml.find_all()])
+    if os.environ['RUN_ENV'] in { 'mock', 'develop' }:
+        app.app.logger.debug(res)
+    return res
