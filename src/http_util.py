ff0432831b381cb984abaa7c6caae7902cd67eb8
Gscienty <gaoxiaochuan@hotmail.com>
2019-03-25 11:18:40 -0400

    fix normal

--- src/http_util.py
+++ src/http_util.py
@@ -7,18 +7,23 @@ __headers = { 'Content-Type': 'application/xml' }
 
 def normal_call(uri, content):
     global __headers
-    res = requests.post(uri,
-                        data=xml_util.to_xml(content),
-                        headers=__headers)
-    return xml_util.parse_xml(res.text)
+    run_env = os.environ['RUN_ENV']
+    if run_env in { 'release', 'develop' }:
+        res = requests.post(uri,
+                data=xml_util.to_xml(content),
+                headers=__headers)
+        return xml_util.parse_xml(res.text)
+    elif run_env in { 'mock' }:
+        return wechat_sec.mock_res_build()
 
 def security_call(uri, content):
     global __headers
-    if os.environ['RUN_ENV'] in { 'release', 'develop' }:
+    run_env = os.environ['RUN_ENV']
+    if run_env in { 'release', 'develop' }:
         res = requests.post(uri,
                 data=xml_util.to_xml(content),
                 headers=__headers,
                 cert=(os.environ['API_CERT'], os.environ['API_KEY']))
         return xml_util.parse_xml(res.text)
-    elif os.environ['RUN_ENV'] in { 'mock' }:
+    elif run_env in { 'mock' }:
         return wechat_sec.mock_res_build()
